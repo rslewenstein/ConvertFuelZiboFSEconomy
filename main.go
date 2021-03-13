a1f7a741acf339d3a3bd6f7633252dd1875b41a0
rslewenstein <rafasoaresjf@gmail.com>
2021-03-13 01:04:12 -0300

    Changes in output

--- main.go
+++ main.go
@@ -2,52 +2,53 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-var refOneGalInPound float32 = 6.8 // On Gal is equals 6.8 pounds (In simulator with the Zibo)
-var refPoundToKg float32 = 2.205   // Tax to calculate between Kile and pounds
+var refOneGalInPound float64 = 6.8 // One Gal is equals 6.8 pounds (In simulator with the Zibo)
+var refPoundToKg float64 = 2.205   // Tax to calculate between Kile and pounds
 
-func GalToPound(gal float32) string {
+func GalToPound(gal float64) string {
 	var msg string
 
 	var valTot = refOneGalInPound * gal
-	msg = "Total of Gallons to Pounds is: " + fmt.Sprint(valTot) + " lb"
+	msg = "Total of Gallons to Pounds is: " + fmt.Sprint(math.Floor(valTot*100)/100) + " lb"
 
 	return msg
 }
 
-func GalToKilo(gal float32) string {
+func GalToKilo(gal float64) string {
 	var msg string
 
 	var valTot = refOneGalInPound * gal
 	valTot = valTot / refPoundToKg
-	msg = "Total of Gallons to Kile is: " + fmt.Sprint(valTot) + " kg"
+	msg = "Total of Gallons to Kile is: " + fmt.Sprint(math.Floor(valTot*100)/100) + " kg"
 
 	return msg
 }
 
-func KileToGal(kg float32) string {
+func KileToGal(kg float64) string {
 	var msg string
 
 	var valTot = kg / refOneGalInPound
 	valTot = valTot * refPoundToKg
-	msg = "Total of Kile to Gallons is: " + fmt.Sprint(valTot) + " gal"
+	msg = "Total of Kile to Gallons is: " + fmt.Sprint(math.Floor(valTot*100)/100) + " gal"
 
 	return msg
 }
 
-func PoundToGal(pound float32) string {
+func PoundToGal(pound float64) string {
 	var msg string
 
 	var valTot = pound / refOneGalInPound
-	msg = "Total of Pounds to Gallons is: " + fmt.Sprint(valTot) + " gal"
+	msg = "Total of Pounds to Gallons is: " + fmt.Sprint(math.Floor(valTot*100)/100) + " gal"
 
 	return msg
 }
 
 func main() {
 
-	var gal, pounds, kg float32
+	var gal, pounds, kg float64
 
 	fmt.Print("Enter the number of gallons (Ex.: 4800.50 or 7000): ")
 	fmt.Scanln(&gal)
